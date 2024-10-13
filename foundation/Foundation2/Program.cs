113ddd2aa97190ddaa111e3271eeb05651b13534
CalebJH1 <cjheilmann@outlook.com>
2024-10-12 21:04:23 -0700

    commit

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -4,54 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Order order1 = new Order();
+        Order orderA = new Order();
 
-        Customer customer1 = new Customer("Noah");
-        order1.SetCustomer(customer1);
+        Customer customerA = new Customer("Noah");
+        orderA.SetCustomer(customerA);
 
-        Address address1 = new Address("Gresham", "Oregon", "USA");
-        customer1.SetAddress(address1);
+        Address addressA = new Address("Gresham", "Oregon", "USA");
+        customerA.SetAddress(addressA);
 
-        Product product1a = new Product("Dog Food", 75293, 5, 2);
-        order1.SetProductList(product1a);
+        Product productA1 = new Product("Dog Food", 75293, 5, 2);
+        orderA.SetProductList(productA1);
 
-        Product product1b = new Product("Milk", 89245, 4, 1);
-        order1.SetProductList(product1b);
+        Product productA2 = new Product("Milk", 89245, 4, 1);
+        orderA.SetProductList(productA2);
 
-        Product product1c = new Product("Candy", 83325, 2, 5);
-        order1.SetProductList(product1c);
+        Product productA3 = new Product("Candy", 83325, 2, 5);
+        orderA.SetProductList(productA3);
 
-        Product product1d = new Product("Pizza", 38294, 10, 1);
-        order1.SetProductList(product1d);
+        Product productA4 = new Product("Pizza", 38294, 10, 1);
+        orderA.SetProductList(productA4);
 
-        Console.WriteLine($"{order1.CalculateTotalCost()} dollars");
-        Console.WriteLine(order1.GetPackingLabel());
-        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"{orderA.CalculateTotalCost()} dollars");
+        Console.WriteLine(orderA.GetPackingLabel());
+        Console.WriteLine(orderA.GetShippingLabel());
 
         Console.WriteLine();
 
-        Order order2 = new Order();
+        Order orderB = new Order();
 
-        Customer customer2 = new Customer("Jorge");
-        order2.SetCustomer(customer2);
+        Customer customerB = new Customer("Jorge");
+        orderB.SetCustomer(customerB);
         
-        Address address2 = new Address("Mexico City", "Mexico");
-        customer2.SetAddress(address2);
+        Address addressB = new Address("Mexico City", "Mexico");
+        customerB.SetAddress(addressB);
 
-        Product product2a = new Product("Tortillas", 12468, 5, 3);
-        order2.SetProductList(product2a);
+        Product productB1 = new Product("Tortillas", 12468, 5, 3);
+        orderB.SetProductList(productB1);
 
-        Product product2b = new Product("Meat", 47359, 9, 1);
-        order2.SetProductList(product2b);
+        Product productB2 = new Product("Meat", 47359, 9, 1);
+        orderB.SetProductList(productB2);
 
-        Product product2c = new Product("Seasoning", 23984, 5, 2);
-        order2.SetProductList(product2c);
+        Product productB3 = new Product("Seasoning", 23984, 5, 2);
+        orderB.SetProductList(productB3);
 
-        Product product2d = new Product("Cheese", 38294, 3, 2);
-        order2.SetProductList(product2d);
+        Product productB4 = new Product("Cheese", 38294, 3, 2);
+        orderB.SetProductList(productB4);
 
-        Console.WriteLine($"{order2.CalculateTotalCost()} dollars");
-        Console.WriteLine(order2.GetPackingLabel());
-        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"{orderB.CalculateTotalCost()} dollars");
+        Console.WriteLine(orderB.GetPackingLabel());
+        Console.WriteLine(orderB.GetShippingLabel());
     }
 }
