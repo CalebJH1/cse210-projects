2c3f14f808a01f0fb82563cfa5b1efbf9f1b519e
CalebJH1 <cjheilmann@outlook.com>
2024-10-15 16:56:52 -0700

    commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("Noah", "Art");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Jacob", "Math", "Order of Operations", "1-10");
+        string homeworkList = mathAssignment.GetHomeworkList();
+        Console.WriteLine(homeworkList);
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Gavin", "English", "The best day ever!");
+        string writingInformation = writingAssignment.GetWritingInformation();
+        Console.WriteLine(writingInformation);
     }
 }
