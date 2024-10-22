88085ec8538091b8a6c7df1fe20b74b4e3dfd947
CalebJH1 <cjheilmann@outlook.com>
2024-10-22 16:08:59 -0700

    commit

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue", 8);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 3, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Red", 7);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color: {color}, Area: {area}");
+        }
     }
 }
