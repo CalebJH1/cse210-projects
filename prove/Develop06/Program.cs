17883f9673174a9c6c834d2e3a7997fe39a8dc1f
CalebJH1 <cjheilmann@outlook.com>
2024-10-28 16:18:06 -0700

    commit

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,3 +1,6 @@
+// To show creativity and exceed core requirements, I've implemented a leveling up system into my program. In the beginning of the program, 
+// the user is asked the number of points needed for each level up.
+
 using System;
 using System.Runtime.InteropServices;
 
